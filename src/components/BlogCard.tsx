b4a16dc64d78c821c4834b8587017a05e7f87877
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-06-04 17:27:51 +0430

    add types for blogs

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { IBlog } from "../types/blog.types";
 
-function BlogCardComponent(){
+function BlogCardComponent() {
+  const [blog, setBlog] = useState<IBlog[]>([
+    {
+      title: "React Tailwind Card with Grid 1",
+      author: "Erfan gholami",
+      image: "https://picsum.photos/536/354",
+      text: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable text",
+    },
+    {
+      title: "React Tailwind Card with Grid 2",
+      image: "https://picsum.photos/id/237/536/354",
+      author: "amir slt",
+      text: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable text",
+    },
+    {
+      title: "React Tailwind Card with Grid 3",
+      image: "https://picsum.photos/seed/picsum/536/354",
+      author: "dina molavi",
+      text: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable text",
+    },
+    {
+      title: "React Tailwind Card with Grid 4",
+      image: "https://picsum.photos/id/1060/536/354?blur=2",
+      author: "ali farj",
+      text: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable text",
+    },
+  ]);
+
   return (
     <>
       <div className="w-full shadow-md lg:max-w-sm p-2">
@@ -9,10 +38,7 @@ function BlogCardComponent(){
           alt="image"
         />
         <div className="p-4">
-       
-          <button className="px-2 py-2 text-sm text-black-300">
-            
-          </button>
+          <button className="px-2 py-2 text-sm text-black-300"></button>
           <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
             Read more
           </button>
@@ -20,5 +46,5 @@ function BlogCardComponent(){
       </div>
     </>
   );
-};
+}
 export default BlogCardComponent;
---
+++ src/types/blog.types.ts
@@ -0,0 +1,6 @@
+export interface IBlog {
+  title: string;
+  image: string;
+  text: string;
+  author: string;
+}
